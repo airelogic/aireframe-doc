544244284e4f18fa806006c418267bbed90a1525
Danny Heard <danny.heard@airelogic.com>
2024-05-23 11:33:19 +0000

    Website auto build on 05/23/2024 11:33:19

--- build-scripts/build.ps1
+++
@@ -1,4 +0,0 @@
-gem install bundler:2.1.4 
-bundle install
-$env:JEKYLL_ENV = 'production'; bundle exec jekyll build -t  --config _config.yml
-#bundle exec htmlproofer ./_site --url-ignore "/#.*/,/http://.*/,/https://.*/"
